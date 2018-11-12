356fe3163079c8d751e28de3812933eacbbcd4dd
GustavoDeCamargo <guustavocamargo@gmail.com>
2018-11-12 16:28:04 -0200

    Projeto Atualizado

--- src/main/java/guru/springframework/WebServiceApplication.java
+++ src/main/java/guru/springframework/WebServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+//testando o commit
 @SpringBootApplication
 public class WebServiceApplication {
 
